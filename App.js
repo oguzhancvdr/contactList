65a49abe8aca1c31140e88db9a5822ec6a9262eb
ogzhn <oguzhancavdar06@gmail.com>
2021-08-05 22:12:30 +0300

    create context API

--- App.js
+++ App.js
@@ -9,9 +9,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import AppNavContainer from './src/navigations';
+import GlobalProvider from './src/context/Provider';
 
 const App = () => {
-  return <AppNavContainer />;
+  return (
+    <GlobalProvider>
+      <AppNavContainer />
+    </GlobalProvider>
+  );
 };
 
 export default App;
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,1 +1,23 @@
-export default () => null;
+import React, {createContext, useReducer} from 'react';
+import authInitialState from './initialStates/authInitialState';
+import contactsInitialState from './initialStates/contactsInitialState';
+import auth from './reducers/auth';
+import contacts from './reducers/contacts';
+
+export const GlobalContext = createContext({});
+
+const GlobalProvider = ({children}) => {
+  const [authState, authDispatch] = useReducer(auth, authInitialState);
+  const [contactsState, contactsDispatch] = useReducer(
+    contacts,
+    contactsInitialState,
+  );
+
+  return (
+    <GlobalContext.Provider
+      value={{authState, authDispatch, contactsState, contactsDispatch}}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+export default GlobalProvider;
