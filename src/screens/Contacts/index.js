0f34cfd67e76c9868841fc54c5ea505178165b2e
ogzhn <oguzhancavdar06@gmail.com>
2021-08-05 23:10:37 +0300

    create a container component

--- src/screens/Contacts/index.js
+++ src/screens/Contacts/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text} from 'react-native';
+import Container from '../../components/common/Container';
 
 const Contacts = () => {
   return (
-    <View>
+    <Container>
       <Text>Hello from Contact Page</Text>
-    </View>
+    </Container>
   );
 };
 
