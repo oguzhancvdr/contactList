2fbaa992f2a40164cc9a8152ded26bad49354f9a
ogzhn <oguzhancavdar06@gmail.com>
2021-08-10 01:43:47 +0300

    create custom button

---
+++ src/components/common/CustomButton/index.js
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import {
+  View,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
+import {color} from 'react-native-reanimated';
+import colors from '../../../assets/theme/colors';
+import styles from './styles';
+
+const CustomButton = ({
+  title,
+  disabled,
+  loading,
+  secondary,
+  primary,
+  danger,
+  onPress,
+  ...props // it should be at the last step
+}) => {
+  const getBgColor = () => {
+    if (disabled) {
+      return colors.grey;
+    }
+    if (primary) {
+      return colors.primary;
+    }
+    if (danger) {
+      return colors.danger;
+    }
+    if (secondary) {
+      return colors.secondary;
+    }
+  };
+  return (
+    <TouchableOpacity
+      disabled={disabled}
+      onPress={onPress}
+      style={[styles.wrapper, {backgroundColor: getBgColor()}]}>
+      <View style={[styles.loaderSection]}>
+        {loading && <ActivityIndicator color={primary ? colors.secondary : colors.primary} />}
+        {title && (
+          <Text
+            style={{
+              color: disabled ? 'black' : colors.white,
+              paddingLeft: loading ? 5 : 0,
+            }}>
+            {title}
+          </Text>
+        )}
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
