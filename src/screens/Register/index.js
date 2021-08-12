d04cb2ea8f566ff57d2535c631190b28073aa9a1
ogzhn <oguzhancavdar06@gmail.com>
2021-08-12 17:23:37 +0300

    control if fields are valid or not

--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,9 +1,73 @@
-import React from 'react';
+import React, {useState} from 'react';
 import RegisterComponent from '../../components/Signup';
 
 const Register = () => {
+  const [form, setForm] = useState({});
+  const [errors, setErrors] = useState({});
+
+  const onChange = ({name, value}) => {
+    setForm({...form, [name]: value});
+
+    if (value !== '') {
+      if (name === 'password') {
+        if (value.length < 6) {
+          setErrors(prev => {
+            return {...prev, [name]: 'This filed needs more than 5 characters'};
+          });
+        } else {
+          setErrors(prev => {
+            return {...prev, [name]: null};
+          });
+        }
+      } else {
+        setErrors(prev => {
+          return {...prev, [name]: null};
+        });
+      }
+    } else {
+      setErrors(prev => {
+        return {...prev, [name]: 'This field is required'};
+      });
+    }
+  };
+
+  const onSubmit = () => {
+    // validations
+    console.log('form :>> ', form);
+    if (!form.userName) {
+      setErrors(prev => {
+        return {...prev, userName: 'Please add a username'};
+      });
+    }
+    if (!form.firstName) {
+      setErrors(prev => {
+        return {...prev, firstName: 'Please add a first name'};
+      });
+    }
+    if (!form.lastName) {
+      setErrors(prev => {
+        return {...prev, lastName: 'Please add a last name'};
+      });
+    }
+    if (!form.email) {
+      setErrors(prev => {
+        return {...prev, email: 'Please add a email'};
+      });
+    }
+    if (!form.password) {
+      setErrors(prev => {
+        return {...prev, password: 'Please add a password'};
+      });
+    }
+  };
+
   return (
-      <RegisterComponent />
+    <RegisterComponent
+      onChange={onChange}
+      onSubmit={onSubmit}
+      form={form}
+      errors={errors}
+    />
   );
 };
 
