85bd530eb0c9e25133a2585780161e7db2154388
ogzhn <oguzhancavdar06@gmail.com>
2021-08-12 16:43:27 +0300

    apply styles for register ui

---
+++ src/components/Signup/styles.js
@@ -0,0 +1,38 @@
+import {StyleSheet} from 'react-native';
+import colors from '../../assets/theme/colors';
+
+export default StyleSheet.create({
+  logoImage: {
+    height: 150,
+    width: 150,
+    alignSelf: 'center',
+    marginTop: 50,
+  },
+  title: {
+    fontSize: 21,
+    textAlign: 'center',
+    paddingTop: 20,
+    fontWeight: '500',
+  },
+  subTitle: {
+    fontSize: 17,
+    textAlign: 'center',
+    paddingVertical: 20,
+    fontWeight: '500',
+  },
+  form: {
+    paddingTop: 20,
+  },
+  createSection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  linkBtn: {
+    paddingLeft: 10,
+    color: colors.primary,
+    fontSize: 16,
+  },
+  infoText: {
+    fontSize: 17,
+  },
+});
