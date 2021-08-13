455e08b87d7057ad6f8277e098f6638b95c4e37f
ogzhn <oguzhancavdar06@gmail.com>
2021-08-13 16:02:56 +0300

    manageauthStates according to case like success, fail

--- src/context/reducers/auth.js
+++ src/context/reducers/auth.js
@@ -1,8 +1,37 @@
+import {
+  CLEAR_AUTH_STATE,
+  REGISTER_FAIL,
+  REGISTER_LOADING,
+  REGISTER_SUCCESS,
+} from '../../constants/actionTypes';
+
 const auth = (state, {type, payload}) => {
   switch (type) {
-    case 'LOGIN':
-      return state;
+    case REGISTER_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: payload,
+      };
+    case REGISTER_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: payload,
+      };
 
+    case CLEAR_AUTH_STATE:
+      return {
+        ...state,
+        loading: false,
+        data: null,
+        error: null,
+      };
     default:
       return state;
   }
