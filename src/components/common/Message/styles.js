f3e3b3e301b47f8446c564e971d398745822347a
ogzhn <oguzhancavdar06@gmail.com>
2021-08-14 13:54:30 +0300

    apply styles for Message comp

---
+++ src/components/common/Message/styles.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+import colors from '../../../assets/theme/colors';
+
+export default StyleSheet.create({
+  wrapper: {
+    height: 42,
+    paddingHorizontal: 5,
+    marginVertical: 5,
+    borderRadius: 4,
+    justifyContent: 'center',
+  },
+  textInput: {
+    flex: 1,
+    width: '100%',
+  },
+  error: {
+    color: colors.danger,
+    paddingTop: 4,
+    fontSize: 12,
+  },
+});
