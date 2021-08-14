3ab486bbd87cd56500d6eb845b8628a08b5774df
ogzhn <oguzhancavdar06@gmail.com>
2021-08-14 15:53:43 +0300

    modfiy login comp

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import LoginComponent from '../../components/Login';
+import {GlobalContext} from '../../context/Provider';
+import loginUser from '../../context/actions/auth/loginUser';
 
 const Login = () => {
-  const [value, onChangeText] = React.useState('');
-  return <LoginComponent />;
+  const [form, setForm] = useState({});
+  const {
+    authDispatch,
+    authState: {error, loading},
+  } = useContext(GlobalContext);
+
+  const onSubmit = () => {
+    if (form.userName && form.password) {
+      loginUser(form)(authDispatch);
+    }
+  };
+  const onChange = ({name, value}) => {
+    setForm({...form, [name]: value});
+  };
+
+  return (
+    <LoginComponent
+      onChange={onChange}
+      onSubmit={onSubmit}
+      form={form}
+      error={error}
+      loading={loading}
+    />
+  );
 };
 
 export default Login;
