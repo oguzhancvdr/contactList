ad40a7a3be773c7bce235e3c313e04d7e4c2f2b0
ogzhn <oguzhancavdar06@gmail.com>
2021-08-20 20:01:39 +0300

    add logout user constant variable

--- src/constants/actionTypes/index.js
+++ src/constants/actionTypes/index.js
@@ -5,3 +5,4 @@ export const LOGIN_LOADING = 'LOGIN_LOADING';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const CLEAR_AUTH_STATE = 'CLEAR_AUTH_STATE';
+export const LOGOUT_USER = 'LOGOUT_USER';
