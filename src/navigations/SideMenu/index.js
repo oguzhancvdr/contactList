d386ae649e082635a469bbcd902289cfcabfff7d
ogzhn <oguzhancavdar06@gmail.com>
2021-08-20 19:26:45 +0300

    implement alert modal when user try to logout

--- src/navigations/SideMenu/index.js
+++ src/navigations/SideMenu/index.js
@@ -1,18 +1,30 @@
-import {loadOptions} from '@babel/core';
 import React from 'react';
-import {View, Image, Text, TouchableOpacity} from 'react-native';
+import {View, Image, Text, TouchableOpacity, Alert} from 'react-native';
 import Container from '../../components/common/Container';
 import {SETTINGS} from '../../constants/routeNames';
 import styles from './styles';
 
 const SideMenu = ({navigation}) => {
+  const handleLogout = () => {
+    navigation.toggleDrawer();
+    Alert.alert('Logout!', 'Are you sure you want to logout?', [
+      {
+        text: 'Cancel',
+        onPress: () => {},
+      },
+      {
+        text: 'OK',
+        onPress: () => {},
+      },
+    ]);
+  };
   const menuItems = [
     {
       icon: <Text>T</Text>,
       name: 'Settings',
       onPress: () => navigation.navigate(SETTINGS),
     },
-    {icon: <Text>T</Text>, name: 'Logout', onPress: () => null},
+    {icon: <Text>T</Text>, name: 'Logout', onPress: () => handleLogout()},
   ];
   return (
     <View>
