08273ca98c1083f4de89821fa2b131c30d875d10
ogzhn <oguzhancavdar06@gmail.com>
2021-08-20 19:19:12 +0300

    create toggle function for drawer

--- src/screens/Contacts/index.js
+++ src/screens/Contacts/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import {Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {Text, TouchableOpacity} from 'react-native';
 import Container from '../../components/common/Container';
 
 const Contacts = () => {
+  const {setOptions, toggleDrawer} = useNavigation();
+  useEffect(() => {
+    setOptions({
+      headerLeft: () => (
+        <TouchableOpacity onPress={() => toggleDrawer()}>
+          <Text style={{padding: 10}}>NAV</Text>
+        </TouchableOpacity>
+      ),
+    });
+  }, []);
   return (
     <Container>
       <Text>Hello from Contact Page</Text>
