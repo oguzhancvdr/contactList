80e18f1dd117d12b1f283d2a68305e48ef72f352
ogzhn <oguzhancavdar06@gmail.com>
2021-08-21 22:52:09 +0300

    catch user info using useRoute hook

--- src/context/actions/auth/register.js
+++ src/context/actions/auth/register.js
@@ -19,7 +19,8 @@ export default ({
     firstName: first_name,
     lastName: last_name,
   }) =>
-  dispatch => {
+  dispatch =>
+  onSuccess => {
     dispatch({
       type: REGISTER_LOADING,
     });
@@ -36,6 +37,7 @@ export default ({
           type: REGISTER_SUCCESS,
           payload: res.data,
         });
+        onSuccess(res.data);
       })
       .catch(err => {
         dispatch({
