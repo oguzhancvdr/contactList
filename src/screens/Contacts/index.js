bfceecd468f3b8195dd467f9110d89836c803f95
ogzhn <oguzhancavdar06@gmail.com>
2021-08-24 00:31:09 +0300

    fetch dumy users and show on the ui

--- src/screens/Contacts/index.js
+++ src/screens/Contacts/index.js
@@ -1,12 +1,26 @@
 import {useNavigation} from '@react-navigation/native';
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {TouchableOpacity} from 'react-native';
 import Icon from '../../components/common/Icon';
 import ContactsComponent from '../../components/ContactsComponent';
+import getContacts from '../../context/actions/contacts/getContacts';
+import {GlobalContext} from '../../context/Provider';
 
 const Contacts = () => {
   const {setOptions, toggleDrawer} = useNavigation();
   const [modalVisible, setModalVisible] = useState(false);
+
+  const {
+    contactsDispatch,
+    contactsState: {
+      getContacts: {data, loading},
+    },
+  } = useContext(GlobalContext);
+
+  useEffect(() => {
+    getContacts()(contactsDispatch);
+  }, []);
+
   useEffect(() => {
     setOptions({
       headerLeft: () => (
@@ -27,6 +41,8 @@ const Contacts = () => {
     <ContactsComponent
       modalVisible={modalVisible}
       setModalVisible={setModalVisible}
+      data={data}
+      loading={loading}
     />
   );
 };
