168bd6aef9fbfafe14564228e5242834eeef7198
ogzhn <oguzhancavdar06@gmail.com>
2021-08-24 00:30:35 +0300

    modify reducers to handle global states

--- src/context/reducers/contacts.js
+++ src/context/reducers/contacts.js
@@ -1,7 +1,39 @@
+import {
+  GET_CONTACTS_FAIL,
+  GET_CONTACTS_LOADING,
+  GET_CONTACTS_SUCCESS,
+} from '../../constants/actionTypes';
+
 const contacts = (state, {type, payload}) => {
   switch (type) {
-    case 'GET_CONTACTS':
-      return state;
+    case GET_CONTACTS_LOADING:
+      return {
+        ...state,
+        getContacts: {
+          ...state.getContacts,
+          loading: true,
+          error: null,
+        },
+      };
+    case GET_CONTACTS_SUCCESS:
+      return {
+        ...state,
+        getContacts: {
+          ...state.getContacts,
+          loading: false,
+          data: payload,
+          error: null,
+        },
+      };
+    case GET_CONTACTS_FAIL:
+      return {
+        ...state,
+        getContacts: {
+          ...state.getContacts,
+          loading: false,
+          error: payload,
+        },
+      };
 
     default:
       return state;
