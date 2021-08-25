6288b3c7973ba710e89b4a90be31e653b4ebbd38
ogzhn <oguzhancavdar06@gmail.com>
2021-08-25 23:30:52 +0300

    correct route name with typo

--- src/constants/routeNames.js
+++ src/constants/routeNames.js
@@ -1,6 +1,6 @@
 export const CONTACT_LIST = 'Contacts';
 export const CONTACT_DETAIL = 'Contact detail';
-export const CREATE_CONTANCT = 'Create contact';
+export const CREATE_CONTACT = 'Create Contact';
 export const SETTINGS = 'Settings';
 export const LOGIN = 'Login';
 export const REGISTER = 'Register';
--- src/navigations/HomeNavigator.js
+++ src/navigations/HomeNavigator.js
@@ -3,7 +3,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {
   CONTACT_DETAIL,
   CONTACT_LIST,
-  CREATE_CONTANCT,
+  CREATE_CONTACT,
   SETTINGS,
 } from '../constants/routeNames';
 import Contacts from '../screens/Contacts';
@@ -17,7 +17,7 @@ const HomeNavigator = () => {
     <HomeStack.Navigator initialRouteName={CONTACT_LIST}>
       <HomeStack.Screen name={CONTACT_LIST} component={Contacts} />
       <HomeStack.Screen name={CONTACT_DETAIL} component={ContactDetail} />
-      <HomeStack.Screen name={CREATE_CONTANCT} component={CreateContact} />
+      <HomeStack.Screen name={CREATE_CONTACT} component={CreateContact} />
       <HomeStack.Screen name={SETTINGS} component={Settings} />
     </HomeStack.Navigator>
   );
