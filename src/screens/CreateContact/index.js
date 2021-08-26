77baa8f834529bb3c77d846608b61310c1277c89
ogzhn <oguzhancavdar06@gmail.com>
2021-08-27 01:37:26 +0300

    update CreateContact.js comp

--- src/screens/CreateContact/index.js
+++ src/screens/CreateContact/index.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
+import {useNavigation} from '@react-navigation/native';
 import CreateContactComponent from '../../components/CreateContact';
+import createContact from '../../context/actions/contacts/createContact';
+import {GlobalContext} from '../../context/Provider';
+import {CONTACT_LIST} from '../../constants/routeNames';
 
 const CreateContact = () => {
-  return <CreateContactComponent />;
+  const {
+    contactsDispatch,
+    contactsState: {
+      createContact: {loading, error},
+    },
+  } = useContext(GlobalContext);
+  const [form, setForm] = useState({});
+  const {navigate} = useNavigation();
+
+  const onChangeText = ({name, value}) => {
+    setForm({...form, [name]: value});
+  };
+
+  const onSubmit = () => {
+    createContact(form)(contactsDispatch)(() => navigate(CONTACT_LIST));
+  };
+
+  return (
+    <CreateContactComponent
+      onChangeText={onChangeText}
+      form={form}
+      setForm={setForm}
+      onSubmit={onSubmit}
+      loading={loading}
+      error={error}
+    />
+  );
 };
 
 export default CreateContact;
