5054577b2eca91b18a851cfa05a283c5511cbbea
ogzhn <oguzhancavdar06@gmail.com>
2021-08-27 20:46:58 +0300

    make dynamic value of isFavorite

--- src/context/actions/contacts/createContact.js
+++ src/context/actions/contacts/createContact.js
@@ -12,7 +12,7 @@ export default form => dispatch => onSuccess => {
     last_name: form.lastName || '',
     phone_number: form.phoneNumber || '',
     contact_picture: form.contactPicture || null,
-    is_favorite: false,
+    is_favorite: form.isFavorite || false,
   };
   dispatch({
     type: CREATE_CONTACT_LOADING,
