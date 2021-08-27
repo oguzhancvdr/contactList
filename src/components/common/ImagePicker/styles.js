fbb213e2f4cf7de0f162cc6dd19d08f8713607f3
ogzhn <oguzhancavdar06@gmail.com>
2021-08-27 21:28:35 +0300

    apply styles for ImagePicker Comp

---
+++ src/components/common/ImagePicker/styles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  optionsWrapper: {
+    paddingHorizontal: 20,
+  },
+  pickerOption: {
+    flexDirection: 'row',
+    paddingTop: 20,
+    alignItems: 'center',
+  },
+  text: {
+    paddingHorizontal: 17,
+    fontSize: 17,
+  },
+});
