e19e1f2e948fc65d92de1fd5db6bac80ad5f7f99
ogzhn <oguzhancavdar06@gmail.com>
2021-08-30 14:29:07 +0300

    add preferences to settings screen

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -2,18 +2,55 @@ import React from 'react';
 import {View, Text, TouchableOpacity, ScrollView} from 'react-native';
 import Container from '../../components/common/Container';
 import styles from './styles';
+import AppModal from '../common/Modal';
+import Icon from '../common/Icon';
 
-const SettingsCompenent = ({settingsOptions}) => {
+const SettingsCompenent = ({
+  settingsOptions,
+  setModalVisible,
+  modalVisible,
+  preferences,
+}) => {
   const options = settingsOptions.map(({title, subTitle, onPress}, i) => (
-    <ScrollView key={title + i} style={styles.settingsContainer}>
-      <TouchableOpacity>
-        <View style={styles.itemContainer}>
-          <Text style={styles.settingsTitle}>{title}</Text>
-          {subTitle && <Text style={styles.settingsSubTitle}>{subTitle}</Text>}
-        </View>
-        <View style={styles.fakeView} />
-      </TouchableOpacity>
-    </ScrollView>
+    <View key={title + i}>
+      <AppModal
+        modalVisible={modalVisible}
+        modalFooter={<></>}
+        setModalVisible={setModalVisible}
+        modalBody={
+          <View>
+            {preferences.map(({name, selected, onPress}, index) => (
+              <View key={name + index}>
+                <TouchableOpacity
+                  style={styles.modalBodyContainer}
+                  onPress={onPress}>
+                  {selected && <Icon name="check" type="ant" size={17} />}
+                  <Text
+                    style={[
+                      styles.settingsTitle,
+                      {paddingLeft: selected ? 10 : 26.7},
+                    ]}>
+                    {name}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            ))}
+          </View>
+        }
+        title="Sort by"
+      />
+      <ScrollView style={styles.settingsContainer}>
+        <TouchableOpacity onPress={onPress}>
+          <View style={styles.itemContainer}>
+            <Text style={styles.settingsTitle}>{title}</Text>
+            {subTitle && (
+              <Text style={styles.settingsSubTitle}>{subTitle}</Text>
+            )}
+          </View>
+          <View style={styles.fakeView} />
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
   ));
 
   return <Container>{options}</Container>;
