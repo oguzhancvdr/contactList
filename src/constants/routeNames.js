db623d5e939fc557fa5d33c81fab2c8f3dcab64a
ogzhn <oguzhancavdar06@gmail.com>
2021-08-31 16:08:07 +0300

    create constant route name for logout

--- src/constants/routeNames.js
+++ src/constants/routeNames.js
@@ -3,5 +3,6 @@ export const CONTACT_DETAIL = 'Contact detail';
 export const CREATE_CONTACT = 'Create Contact';
 export const SETTINGS = 'Settings';
 export const LOGIN = 'Login';
+export const LOGOUT = 'LOGOUT';
 export const REGISTER = 'Register';
 export const HOME_NAVIGATOR = 'Home';
