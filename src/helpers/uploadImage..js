5fb70b6a804eeff9d8204824189c7ac21af8444e
ogzhn <oguzhancavdar06@gmail.com>
2021-08-31 17:31:31 +0300

    create uploadImage helper that handles with fireabse storage and create image

---
+++ src/helpers/uploadImage..js
@@ -0,0 +1,16 @@
+import storage from '@react-native-firebase/storage';
+
+export default file => onSuccess => onError => {
+  const path = 'contact-pictures/user/777/' + file.creationDate || file.path;
+  const ref = storage().ref(path);
+  const task = ref.putFile(file.path);
+
+  task
+    .then(async () => {
+      const url = await ref.getDownloadURL();
+      onSuccess(url);
+    })
+    .catch(err => {
+      onError(err);
+    });
+};
