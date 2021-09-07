c0efc0c2c537a1b58194b989870ee23ca12cb932
ogzhn <oguzhancavdar06@gmail.com>
2021-09-07 22:34:25 +0300

    add new feature that users can edit their contacts

--- src/screens/CreateContact/index.js
+++ src/screens/CreateContact/index.js
@@ -1,10 +1,12 @@
-import React, {useState, useContext, useRef} from 'react';
-import {useNavigation} from '@react-navigation/native';
+import React, {useState, useContext, useRef, useEffect} from 'react';
+import {useNavigation, useRoute} from '@react-navigation/native';
 import CreateContactComponent from '../../components/CreateContact';
 import createContact from '../../context/actions/contacts/createContact';
 import {GlobalContext} from '../../context/Provider';
-import {CONTACT_LIST} from '../../constants/routeNames';
+import {CONTACT_DETAIL, CONTACT_LIST} from '../../constants/routeNames';
 import uploadImage from '../../helpers/uploadImage.';
+import countryCodes from '../../utils/countryCodes';
+import editContact from '../../context/actions/contacts/editContact';
 
 const CreateContact = () => {
   const {
@@ -17,8 +19,51 @@ const CreateContact = () => {
   const [form, setForm] = useState({});
   const [isUploading, setIsUploading] = useState(false);
   const [localFile, setLocalFile] = useState(null);
-  const {navigate} = useNavigation();
+  const {navigate, setOptions} = useNavigation();
+  const {params} = useRoute();
   const sheetRef = useRef(null);
+
+  useEffect(() => {
+    if (params?.contact) {
+      setOptions({title: 'Update contact'});
+
+      const {
+        first_name: firstName,
+        last_name: lastName,
+        phone_number: phoneNumber,
+        is_favorite: isFavorite,
+        country_code: countryCode,
+      } = params?.contact;
+
+      setForm(prev => {
+        return {
+          ...prev,
+          firstName,
+          lastName,
+          phoneNumber,
+          isFavorite,
+          phoneCode: countryCode,
+        };
+      });
+
+      const country = countryCodes.find(item => {
+        return item.value.replace('+', '') === countryCode;
+      });
+
+      if (country) {
+        setForm(prev => {
+          return {
+            ...prev,
+            countryCode: country.flag.toUpperCase(),
+          };
+        });
+      }
+
+      if (params?.contact?.contact_picture) {
+        setLocalFile(params?.contact?.contact_picture);
+      }
+    }
+  }, []);
 
   const closeSheet = () => {
     if (sheetRef.current) {
@@ -36,21 +81,44 @@ const CreateContact = () => {
   };
 
   const onSubmit = () => {
-    if (localFile?.size) {
-      setIsUploading(true);
-      uploadImage(localFile)(url => {
-        setIsUploading(false);
-        createContact({...form, contactPicture: url})(contactsDispatch)(() =>
-          navigate(CONTACT_LIST),
-        );
-      })(err => {
-        if (err) {
+    if (params?.contact) {
+      if (localFile?.size) {
+        setIsUploading(true);
+        uploadImage(localFile)(url => {
           setIsUploading(false);
-        }
-      });
+          editContact(
+            {...form, contactPicture: url},
+            params?.contact?.id,
+          )(contactsDispatch)(item => navigate(CONTACT_DETAIL, {item}));
+        })(err => {
+          if (err) {
+            setIsUploading(false);
+          }
+        });
+      } else {
+        editContact(form, params?.contact?.id)(contactsDispatch)(item => {
+          navigate(CONTACT_DETAIL, {item});
+        });
+      }
+    } else {
+      if (localFile?.size) {
+        setIsUploading(true);
+        uploadImage(localFile)(url => {
+          setIsUploading(false);
+          createContact({...form, contactPicture: url})(contactsDispatch)(
+            () => {
+              navigate(CONTACT_LIST);
+            },
+          );
+        })(err => {
+          if (err) {
+            setIsUploading(false);
+          }
+        });
+      } else {
+        createContact(form)(contactsDispatch)(() => navigate(CONTACT_LIST));
+      }
     }
-
-    createContact(form)(contactsDispatch)(() => navigate(CONTACT_LIST));
   };
 
   const toggleValueChange = () => {
