680f0adb9dbe076e6437761886fedde7b79c12b6
ogzhn <oguzhancavdar06@gmail.com>
2021-09-09 23:31:29 +0300

    implement adding picture if users dont have

--- src/components/ContactDetails/index.js
+++ src/components/ContactDetails/index.js
@@ -1,14 +1,24 @@
 import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {ScrollView, Text, View, TouchableOpacity} from 'react-native';
+import {ScrollView, Text, View, TouchableOpacity, Image} from 'react-native';
 import colors from '../../assets/theme/colors';
 import ImageComponent from './ImageComponent';
 import styles from './styles';
 import Icon from '../common/Icon';
 import CustomButton from '../common/CustomButton';
 import {CREATE_CONTACT} from '../../constants/routeNames';
+import {DEFAULT_IMAGE_URI} from '../../constants/general.js';
+import ImagePicker from '../common/ImagePicker';
 
-const ContactDetails = ({contact}) => {
+const ContactDetails = ({
+  contact,
+  localFile,
+  sheetRef,
+  onFileSelected,
+  openSheet,
+  isUploading,
+  isSucceed,
+}) => {
   const {navigate} = useNavigation();
 
   const {contact_picture, first_name, country_code, phone_number, last_name} =
@@ -17,7 +27,27 @@ const ContactDetails = ({contact}) => {
   return (
     <ScrollView style={styles.scrollContainer}>
       <View style={styles.container}>
-        {contact_picture && <ImageComponent src={contact_picture} />}
+        {(contact_picture || isSucceed) && (
+          <ImageComponent src={contact_picture || localFile?.path} />
+        )}
+
+        {!contact_picture && !isSucceed && (
+          <View style={{alignItems: 'center', paddingVertical: 20}}>
+            <Image
+              source={{
+                uri: localFile?.path || DEFAULT_IMAGE_URI,
+              }}
+              width={150}
+              height={150}
+              style={styles.avatar}
+            />
+            <TouchableOpacity onPress={() => openSheet()}>
+              <Text style={{color: colors.primary}}>
+                {isUploading ? 'Updating...' : 'Add picture'}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )}
 
         <View style={styles.content}>
           <Text style={styles.name}>{first_name + ' ' + last_name} </Text>
@@ -91,6 +121,7 @@ const ContactDetails = ({contact}) => {
           }}
         />
       </View>
+      <ImagePicker ref={sheetRef} onFileSelected={onFileSelected} />
     </ScrollView>
   );
 };
